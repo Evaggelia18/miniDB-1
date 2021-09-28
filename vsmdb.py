4cb6821fa959b3998d04b568d89215e16b405353
Panos Triantafyllou <panos30fyllou@gmail.com>
2021-09-28 21:28:04 +0300

    Solution I think

--- vsmdb.py
+++ vsmdb.py
@@ -1,4 +1,6 @@
 from database import Database
+from btree import Btree
+
 # create db with name "smdb"
 db = Database('vsmdb', load=False)
 # create a single table named "classroom"
@@ -10,3 +12,10 @@ db.insert('classroom', ['Taylor', '3128', '70'])
 db.insert('classroom', ['Watson', '100', '30'])
 db.insert('classroom', ['Watson', '120', '50'])
 
+bt = Btree(3)
+bt.insert(5, 0)
+bt.insert(12, 1)
+bt.insert(7, 2)
+bt.insert(3, 3)
+bt.insert(2, 4)
+bt.insert(9, 5)
